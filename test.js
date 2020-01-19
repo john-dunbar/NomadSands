f41b75b3e9fd4870d70cd02cf1a6efc68c06fd98
dun08015 <dun08015@gmail.com>
2020-01-18 16:25:38 -1000

    added css and changed js file to load index.html

--- test.js
+++ test.js
@@ -1,10 +1,20 @@
 // Load the http module to create an http server.
 var http = require('http');
+var fs = require('fs');
  
 // Configure the HTTP server to print Hello World! to all requests.
 var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World!\n");
+    fs.readFile("index.html", function (error, pgResp) {
+            if (error) {
+                response.writeHead(404);
+                response.write('Contents you are looking are Not Found');
+            } else {
+                response.writeHead(200, { 'Content-Type': 'text/html' });
+                response.write(pgResp);
+            }
+             
+            response.end();
+    });
 });
  
 // Listen on port 8080
