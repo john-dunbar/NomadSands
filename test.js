cb39c958764092155f45100fc390a4696502f68a
root <root@ip-172-31-26-227.us-east-2.compute.internal>
2020-01-19 01:38:54 +0000

    test node.js file

---
+++ test.js
@@ -0,0 +1,13 @@
+// Load the http module to create an http server.
+var http = require('http');
+ 
+// Configure the HTTP server to print Hello World! to all requests.
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hello World!\n");
+});
+ 
+// Listen on port 8080
+server.listen(8080);
+ 
+console.log('Server available at http://localhost:8080/');
