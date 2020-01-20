e1082b61e433deea557b99e64bed166eabf1eab8
dun08015 <dun08015@gmail.com>
2020-01-20 00:30:52 -1000

    added code to utilize express functionality

--- test.js
+++ test.js
@@ -1,23 +1,23 @@
-// Load the http module to create an http server.
-var http = require('http');
-var fs = require('fs');
- 
-// Configure the HTTP server to print Hello World! to all requests.
-var server = http.createServer(function (request, response) {
-    fs.readFile("index.html", function (error, pgResp) {
-            if (error) {
-                response.writeHead(404);
-                response.write('Contents you are looking are Not Found');
-            } else {
-                response.writeHead(200, { 'Content-Type': 'text/html' });
-                response.write(pgResp);
-            }
-             
-            response.end();
-    });
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
 });
- 
-// Listen on port 8080
-server.listen(8080);
- 
-console.log('Server available at http://localhost:8080/');
+
+router.get('/about',function(req,res){
+  res.sendFile(path.join(__dirname+'/about.html'));
+});
+
+router.get('/sitemap',function(req,res){
+  res.sendFile(path.join(__dirname+'/sitemap.html'));
+});
+
+//add the router
+app.use('/', router);
+app.listen(process.env.port || 8080);
+
+console.log('Running at Port 8080');
