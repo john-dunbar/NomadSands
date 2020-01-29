59d767338bcaf2759aa5bd5666d0500ac1404e8c
dun08015 <dun08015@gmail.com>
2020-01-28 21:07:05 -1000

    adds simple form and tests post

--- index.js
+++ index.js
@@ -29,13 +29,13 @@ app.route('/login')
 
     // show the form (GET http://localhost:8080/login)
     .get(function(req, res) {
-        res.send('this is the login form');
+        res.send(__dirname+'/login.html');
     })
 
     // process the form (POST http://localhost:8080/login)
     .post(function(req, res) {
-        console.log('processing');
-        res.send('processing the login form!');
+        
+        res.send(__dirname+'/index.html');
     });
 
 //add the router
