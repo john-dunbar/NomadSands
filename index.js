d1c473aea9617852bb8b67d30fd19f5c51c0204a
dun08015 <dun08015@gmail.com>
2020-01-28 20:24:39 -1000

    initial test of login page

--- index.js
+++ index.js
@@ -25,6 +25,19 @@ router.get('/Javascript/menuClick.js',function(req,res){
   res.sendFile(path.join(__dirname+'/Javascript/menuClick.js'));
 });
 
+app.route('/login')
+
+    // show the form (GET http://localhost:8080/login)
+    .get(function(req, res) {
+        res.send('this is the login form');
+    })
+
+    // process the form (POST http://localhost:8080/login)
+    .post(function(req, res) {
+        console.log('processing');
+        res.send('processing the login form!');
+    });
+
 //add the router
 app.use('/', router);
 app.listen(process.env.port || 8080);
