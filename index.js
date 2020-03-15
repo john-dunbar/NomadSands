877e419ff4384d1198eae42aceb3c47bc13bffb2
dun08015 <dun08015@gmail.com>
2020-03-15 11:43:33 -1000

    use saved user token to create Discord guild

--- index.js
+++ index.js
@@ -308,7 +308,7 @@ async function findUser(sessionId) {
             userName: 'CMDR_Hufflepuff'
         }).toArray();
 
-        console.error("result of userFind: " + res.userName);
+        console.error("result of userFind: " + res[0].userName);
 
         return res;
 
