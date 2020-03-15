dbf6c53440ace86bb3bfc5e34375edc9f4cb7179
dun08015 <dun08015@gmail.com>
2020-03-15 09:45:37 -1000

    make Discord user fetch save token info

--- index.js
+++ index.js
@@ -61,16 +61,20 @@ router.get('/oauth/redirect', function (req, res) {
         })
 
         .then(fetchResp => fetchResp.json())
-        .then(tokenData =>
-            fetch('https://discordapp.com/api/users/@me', {
-                headers: {
-                    authorization: `${tokenData.token_type} ${tokenData.access_token}`,
-                },
-            })
+        .then(tokenData => {
+
+                var fetchedUser = fetch('https://discordapp.com/api/users/@me', {
+                    headers: {
+                        authorization: `${tokenData.token_type} ${tokenData.access_token}`,
+                    },
+                });
+                return fetchedUser;
+            }
+
         )
         .then(userData => userData.json())
         .then(data => {
-            console.error("token type: " + tokenData.token_type);
+            //console.error("token type: " + tokenData.token_type);
             req.session.username = data.username;
             req.session.avatar = data.avatar;
             req.session.userId = data.id;
