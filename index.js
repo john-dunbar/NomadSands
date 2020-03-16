66cfcf52b2a74d39241dccef6834b3d77f1d65c3
dun08015 <dun08015@gmail.com>
2020-03-16 01:52:42 -1000

    use saved user token to create Discord guild

--- index.js
+++ index.js
@@ -384,7 +384,9 @@ async function createGuild(sessionId, matchName) {
     guildManager.create(matchName)
         .then(guildData => {
             let data = guildManager.resolve(guildData);
-            data.addMember(user.userId, user.accessToken);
+            data.addMember(user.userId, {
+                options.access_token: user.accessToken
+            });
             console.log("guild data: " + data.name);
             console.log("guild data: " + data.id);
             console.log("guild data: " + data.maximumMembers);
