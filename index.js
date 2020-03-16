a9524e086b4819a4334a2332c9245dec614b884a
dun08015 <dun08015@gmail.com>
2020-03-15 19:44:53 -1000

    use saved user token to create Discord guild

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const client = new discord.Client();
 console.log("bot token: " + process.env.DISCORD_BOT_TOKEN);
 var guildManager = new discord.GuildManager(client);
 client.login(process.env.DISCORD_BOT_TOKEN);
-console.log("bot guilds: " + client.guilds.cache.first(2)[0]);
+console.log("bot guilds: " + guildManager.cache.first(2)[0]);
 
 //path for public files
 const path = require('path');
