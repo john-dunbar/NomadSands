5005c9f0258d8d238bbeea19cf3cf798fd3cd456
dun08015 <dun08015@gmail.com>
2020-04-26 11:13:16 -1000

    add discord server select dropdown

--- public/javascript/clickHandler.js
+++ public/javascript/clickHandler.js
@@ -16,6 +16,7 @@ $("button").on("click", $(this).event, updateRedirect);
 
 function updateRedirect(event) {
 
+    console.log(event.target.id);
     if (event.target.id.includes("userGuildSelect")) {
 
         console.log(event.target.value);
