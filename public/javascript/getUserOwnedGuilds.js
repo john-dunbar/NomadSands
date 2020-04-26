b6064dfc49f871ad7acec3ba50d5ca4be4402eac
dun08015 <dun08015@gmail.com>
2020-04-26 06:59:28 -1000

    add discord server select dropdown

--- public/javascript/getUserOwnedGuilds.js
+++ public/javascript/getUserOwnedGuilds.js
@@ -1,14 +1,4 @@
 function getUserOwnedGuilds() {
     console.log("getting guilds browser");
-    $.ajax({
-        url: "/getUserGuilds",
-        method: "GET",
-        success: function (data) {
-            console.log("getting guilds success!")
-            if (data.length === 0) {
-                let dropDownItem = "<button class=\"dropdown-item\" type=\"button\">" + Please create a Discord server + "</button>";
-                $('#userDiscordServers').append(dropDownItem);
-            }
-        }
-    });
+
 }
