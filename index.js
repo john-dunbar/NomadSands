1d1bfdacaf30986d6cb009b67807763c944086af
dun08015 <dun08015@gmail.com>
2020-04-26 07:28:13 -1000

    add discord server select dropdown

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ router.get('/oauth/redirect', function (req, res) {
                     })
                     .then(userGuilds => userGuilds.json())
                     .then(guilds => {
-                        req.session.guilds = guilds;
+                        req.session.guilds = "test";
                         console.log(req.session.guilds);
                     });
 
