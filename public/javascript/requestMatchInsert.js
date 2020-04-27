37918fd62fa17280824c97483e68c7e5397bbffd
dun08015 <dun08015@gmail.com>
2020-04-27 07:18:30 -1000

    include hidden server id in dropdown

--- public/javascript/requestMatchInsert.js
+++ public/javascript/requestMatchInsert.js
@@ -27,10 +27,10 @@ function requestMatchInsert() {
     }
 
     var discordServerName = $('#dropdownMenu').text();
-    var targetElementID = discordServerName + "ID";
-    var discordServerID = $('#' + targetElementID).val();
+    var targetElementID = "#" + discordServerName + "ID";
+    var discordServerID = $(targetElementID).val();
 
-    console.log("attempted to jquery: " + '#' + targetElementID);
+    console.log("attempted to jquery: " + targetElementID);
     console.log("server id: " + discordServerID);
 
     var formData = new FormData();
