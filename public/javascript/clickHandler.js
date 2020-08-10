e8b7854d8aa0deb807b53d4dd76a681eb70b5967
John Dunbar <dun08015@gmail.com>
2020-08-10 00:12:46 -1000

    debug join button

--- public/javascript/clickHandler.js
+++ public/javascript/clickHandler.js
@@ -1,67 +1,72 @@
-$("#createMatch").click(function (event) {
+$( document ).ready(function() {
 
-    if ($('#dropdownMenu').text() === "Choose Discord Server") {
+    $("#createMatch").click(function (event) {
 
-        $("#selectDiscordServerAlert").collapse('show');
-
-    } else {
-
-        $("#myForm").modal('hide');
-
-        requestMatchInsert();
-
-    }
-
-});
-
-$("#mainMenuCreateMatch").click(function (event) {
-
-    formatDateTime();
-
-    getUserOwnedGuilds();
-
-});
-
-//this part below took a few hours to figure out
-//should be $(parent element).on(click, dynamically generated child element, event params, function call)
-
-$("#userDiscordServers").on("click", ".dropdown-item", $(this).event, updateRedirect);
-
-function updateRedirect(event) {
-
-    if (event.target.id.includes("userGuildSelect")) {
-
-        $("#dropdownMenu").html(event.target.innerText);
-
-        $("#selectDiscordServerAlert").collapse('hide');
-
-    }
-
-}
-
-$("#deleteMatch").click(function (event) {
-
-
-});
-
-document.getElementById("joinMatch").onclick(function () {
-    //later if it's possible to make an invite link, do all this in the requestMatchInsert flow
-    //window.open("https://www.w3schools.com"); 
-    //after clicking, check that user has been added to group before changing label to "leave"
-    console.log("join button clicked!");
-    var guildId = $('#guildId').val();
-    console.log("guildId = "+guildId);
-    var formData = new FormData();
-    formData.append("guildId", guildId);
-    $.ajax({
-        url: "/joinMatch",
-        method: "GET",
-        data: formData, // request is the value of search input
-        success: function (data) {
-            // Map response values to fiedl label and value
-            console.log(data);
-
+        if ($('#dropdownMenu').text() === "Choose Discord Server") {
+    
+            $("#selectDiscordServerAlert").collapse('show');
+    
+        } else {
+    
+            $("#myForm").modal('hide');
+    
+            requestMatchInsert();
+    
         }
+    
     });
-    console.log("back from index.js");
+    
+    $("#mainMenuCreateMatch").click(function (event) {
+    
+        formatDateTime();
+    
+        getUserOwnedGuilds();
+    
+    });
+    
+    //this part below took a few hours to figure out
+    //should be $(parent element).on(click, dynamically generated child element, event params, function call)
+    
+    $("#userDiscordServers").on("click", ".dropdown-item", $(this).event, updateRedirect);
+    
+    function updateRedirect(event) {
+    
+        if (event.target.id.includes("userGuildSelect")) {
+    
+            $("#dropdownMenu").html(event.target.innerText);
+    
+            $("#selectDiscordServerAlert").collapse('hide');
+    
+        }
+    
+    }
+    
+    $("#deleteMatch").click(function (event) {
+    
+    
+    });
+    
+    $("#joinMatch").click(function (event) {
+        //later if it's possible to make an invite link, do all this in the requestMatchInsert flow
+        //window.open("https://www.w3schools.com"); 
+        //after clicking, check that user has been added to group before changing label to "leave"
+        console.log("join button clicked!");
+        var guildId = $('#guildId').val();
+        console.log("guildId = "+guildId);
+        var formData = new FormData();
+        formData.append("guildId", guildId);
+        $.ajax({
+            url: "/joinMatch",
+            method: "GET",
+            data: formData, // request is the value of search input
+            success: function (data) {
+                // Map response values to fiedl label and value
+                console.log(data);
+    
+            }
+        });
+        console.log("back from index.js");
+    });
+
+
 });
