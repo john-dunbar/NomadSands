2a82e7402ce3113bc566c79207d255290074a701
dun08015 <dun08015@gmail.com>
2020-09-06 19:38:46 -1000

    nav menu change for user avatar

--- public/javascript/getAvatar.js
+++ public/javascript/getAvatar.js
@@ -4,12 +4,13 @@ function getUserAvatar() {
         url: "/getUserAvatar",
         method: "GET",
         success: function (data) {
-
+            console.log("here's what i got " + data);
             if (data.length === 0) {
 
                 $('#userAvatar').attr('src', 'img/profileAvatar.svg');
 
             } else {
+                console.log("here's what i got " + data);
 
                 $('#userAvatar').attr('src', data);
 
