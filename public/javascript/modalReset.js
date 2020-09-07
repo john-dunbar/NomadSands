f6b65cc879541d9a9eaca6e61f280aca23c800cf
dun08015 <dun08015@gmail.com>
2020-09-07 11:46:56 -1000

    fix dropdown showing half way

--- public/javascript/modalReset.js
+++ public/javascript/modalReset.js
@@ -1,5 +1,6 @@
 //reset input fields on close of modal
 $('.modal').on('hidden.bs.modal', function () {
     $('#formInputs')[0].reset();
-    //$('.dropdown-menu').children().remove(); this was causeing main menu options to disappear
+    //prevent same discord servers from being appended to the server list each time the modal opens.
+    $('#userDiscordServers').children().remove();
 });
