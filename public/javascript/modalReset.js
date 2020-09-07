dbe511bdcfc38b2ddf2dac82693adfeb2b5d9e50
dun08015 <dun08015@gmail.com>
2020-09-07 11:43:32 -1000

    clean up dropdown css

--- public/javascript/modalReset.js
+++ public/javascript/modalReset.js
@@ -1,5 +1,5 @@
 //reset input fields on close of modal
 $('.modal').on('hidden.bs.modal', function () {
     $('#formInputs')[0].reset();
-    $('.dropdown-menu').children().remove();
+    //$('.dropdown-menu').children().remove(); this was causeing main menu options to disappear
 });
