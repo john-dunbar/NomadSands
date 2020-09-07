5e757df86389e9adbef50f1903d82be7c3fc74c3
dun08015 <dun08015@gmail.com>
2020-09-06 19:53:51 -1000

    nav menu change for user avatar

--- public/javascript/dbGetMatches.js
+++ public/javascript/dbGetMatches.js
@@ -26,7 +26,11 @@ function pageAppendMatchInfo(user, matchRecord) {
     } else {
         joinButton = "<button type=\"button\" class=\"btn btn-primary btn-sm\" id=\"joinMatch\">Join</button>";
     }
-
+    console.log("organizerAvatar " + matchRecord.organizerAvatar);
+    if (matchRecord.organizerAvatar == "") {
+        matchRecord.organizerAvatar = img / profileAvatar.svg;
+        console.log("organizerAvatar " + matchRecord.organizerAvatar);
+    }
     if (matchRecord.gameName) {
         var matchCard =
             "<div class=\"col-12 col-md-3\">" +
