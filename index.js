e003121a78b4517f6a8cb26032a806254d41c0e4
dun08015 <dun08015@gmail.com>
2020-09-12 18:36:24 -1000

    get guild channels

--- index.js
+++ index.js
@@ -167,7 +167,7 @@ router.get('/oauth/redirect', function (req, res) {
                                                 },
                                             })
                                             .then(channels => {
-                                                console.log("returned data " + channels[0])
+                                                console.log("returned data " + channels);
                                                 guild.channels = channels;
                                             });
                                     })
