93eaa938751c17b546ce29a78ba0c4e843ab203b
dun08015 <dun08015@gmail.com>
2020-09-14 00:27:13 -1000

    fix null user

--- public/javascript/clickHandler.js
+++ public/javascript/clickHandler.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
 
                 var userName = "";
 
-                if (typeof (data[0]) != "undefined") {
+                if (data[0] != null) {
                     userName = data[0];
                 } else {
                     console.log("user is guest");
